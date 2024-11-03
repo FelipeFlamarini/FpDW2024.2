23334163b2fc6eb60b98c6c0e9b1f00d55701fb2
Felipe Flamarini <felipeflamarini@hotmail.com>
2024-11-03 19:40:19 +0000

    progresso atividades

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from pathlib import Path
 from tqdm import tqdm
+
+# blueprints aulas
 from aulas.au03.main import au03Blueprint
+
+# blueprints atividades
+from atividades.at01.main import at01Blueprint
 
 app = Flask(__name__)
 
@@ -10,8 +15,12 @@ app = Flask(__name__)
 def directories():
     return {"dir": [str(dir) for dir in tqdm(Path(".").iterdir())]}
 
+
+# blueprints aulas
 app.register_blueprint(au03Blueprint)
 
+# blueprints atividades
+app.register_blueprint(at01Blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- atividades/at01/main.py
+++ atividades/at01/main.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template
 
 at01Blueprint = Blueprint("at01", __name__, url_prefix="/at01")
+
+@at01Blueprint.route("/")
+def index():
+    return render_template("at01/index.html")
